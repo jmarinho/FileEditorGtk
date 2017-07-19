611a3e0b0d259eace20b1b45df89e5e21707a0e4
Jose Marinho <zemanel@gmail.com>
2017-07-19 22:56:14 +0100

    add plot proof of concept

--- plotUi.py
+++ plotUi.py
@@ -9,23 +9,36 @@ def completer(text, state):
 readline.parse_and_bind("tab: complete")
 readline.set_completer(completer)
 
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_gtk3cairo import FigureCanvasGTK3Cairo as FigureCanvas
+
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
 import fileList
+from gi.repository import Gtk
 from gi.repository import GtkSource 
+import numpy as np
 
 win = Gtk.Window(title="mainWindow")
 win.connect("delete-event", Gtk.main_quit)
 
-sourceViewer = GtkSource.View()
 
+win.set_default_size(400, 300)
+f = Figure(figsize=(5, 4), dpi=100)
+a = f.add_subplot(111)
+t = np.arange(0.0, 3.0, 0.01)
+s = np.sin(2*np.pi*t)
+a.plot(t, s)
 
-treeView = fileList.FileList(".", sourceViewer)
+canvas = FigureCanvas(f)  # a gtk.DrawingArea
+sw = Gtk.ScrolledWindow()
+sw.add_with_viewport(canvas)
+win.add(sw)
+
+treeView = fileList.FileList(".", a)
 box = Gtk.Box(spacing = 10)
 box.pack_start(treeView, True, True, 0)
 
-box.pack_start(sourceViewer, True, True, 0)
 
 scrollWindow1 = Gtk.ScrolledWindow()
 scrollWindow1.add(box)
