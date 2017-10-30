c15748aede04e5c5826e9fc0c73ec1ea05c04f3e
Jose Marinho <jose.marinho@arm.com>
2017-10-30 19:10:38 +0000

    implement basic zoom

--- TkPlot.py
+++ TkPlot.py
@@ -12,52 +12,81 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 # Responsible for 
 class PlotGraph:
 
-    def __init__():
-        pass
+    def __init__(self, mainWindow):
+        self.mainWindow = mainWindow
         
-    ## Input file name just beng used for debug sake
-    json_file_name = "plotInput.json" 
-    componentName = "/TopLevel/SoC"
+        ## Input file name just beng used for debug sake
+        json_file_name = "plotInput.json" 
+        componentName = "/TopLevel/SoC"
+        self.figure = Figure(dpi=100)
+        self.subPlot = self.figure.add_subplot(111) # returns the axes
 
-    def tupleToFloatArray(tupleVariable):
+        self.dataXRange
+        self.dataYXRange
+
+        inputData = json.load(open(json_file_name, "r"))
+
+        data = sorted(inputData["traces"][componentName]["total_power"].items(), key = self.tupleToFloatArray)
+
+        dataLen = len(data)
+        self.x = [0]*dataLen
+        self.y = [0]*dataLen
+
+        for index,item in enumerate(data):
+            self.x[index] = item[0]
+            self.y[index] = item[1]
+
+        self.subPlot.plot(self.x, self.y)
+
+        # a tk.DrawingArea
+        self.canvas = FigureCanvasTkAgg(self.figure, master=mainWin)
+        self.canvas.mpl_connect("motion_notify_event", self.motionHandler)
+        self.canvas.mpl_connect("scroll_event", self.scrollHandler)
+        self.canvas.mpl_connect("button_press_event", self.buttonPressHandler)
+        self.canvas.mpl_connect("button_release_event", self.buttonReleaseHandler)
+        self.canvas.show()
+        self.canvas.get_tk_widget().pack( expand=1)
+
+
+    def tupleToFloatArray(self, tupleVariable):
         return float(tupleVariable[0])
 
-    def motionHandler(mouseEvent):
-        print "{0}+, {1}".format(mouseEvent.xdata, mouseEvent.ydata)
+    def motionHandler(self, mouseEvent):
+        print "motion {0}+, {1}".format(mouseEvent.xdata, mouseEvent.ydata)
 
-    mainWin = Tk()
+    def scrollHandler(self, mouseEvent):
+        print "scroll {0}+, {1}".format(mouseEvent.xdata, mouseEvent.ydata)
 
-    mainWin.geometry('651x700+51+51')
-    mainWin.wm_title('')
+    def buttonPressHandler(self, mouseEvent):
+        self.startX = mouseEvent.xdata
+        self.startY = mouseEvent.ydata
+        
+        print "press {0}+, {1}".format(mouseEvent.xdata, mouseEvent.ydata)
 
+    def buttonReleaseHandler(self, mouseEvent):
+        print "release {0}+, {1}".format(mouseEvent.xdata, mouseEvent.ydata)
+    
+        startX = min(self.startX, mouseEvent.xdata)
+        startY = min(self.startY, mouseEvent.ydata)
 
-    figure = Figure(dpi=100)
-    a = figure.add_subplot(111)
+        endX = max(self.startX, mouseEvent.xdata)
+        endY = max(self.startY, mouseEvent.ydata)
 
-    inputData = json.load(open(json_file_name, "r"))
+        self.subPlot.set_xlim([startX, endX]) 
+        self.canvas.draw()
 
-    data = sorted(inputData["traces"][componentName]["total_power"].items(), key = tupleToFloatArray)
+mainWin = Tk()
 
-    dataLen = len(data)
-    x = [0]*dataLen
-    y = [0]*dataLen
-
-    for index,item in enumerate(data):
-        x[index] = item[0]
-        y[index] = item[1]
-
-
-    a.plot(x,y)
-
-    # a tk.DrawingArea
-    canvas = FigureCanvasTkAgg(figure, master=mainWin)
-    canvas.mpl_connect("motion_notify_event", motionHandler)
-    canvas.show()
-    canvas.get_tk_widget().pack( expand=1)
+graph = PlotGraph(mainWin)
 
 
-    mainWin.mainloop()
+mainWin.geometry('651x700+51+51')
+mainWin.wm_title('')
 
-graph = PlotGraph()
+
+
+
+mainWin.mainloop()
+
 
 
